48e05da45c9226996d63a2f55c587aae65f09d3f
Kevin Hou <strawberrypi64@gmail.com>
2025-11-26 11:57:42 +1100

    feat: config serwist

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,20 @@
-const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
-};
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 
-export default nextConfig;
+module.exports = async (phase) => {
+  const nextConfig = {
+    transpilePackages: ["@repo/ui"],
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "lib/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+};
