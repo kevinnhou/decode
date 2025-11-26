03d2426a701e704028df850ceb458f2dc1db193a
kevin <109639193+kevinnhou@users.noreply.github.com>
2025-11-26 11:59:01 +1100

    feat: setup pwa (#7)

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -13,6 +13,11 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/favicon.ico",
         type: "image/x-icon",
       },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
     name: site.name,
     orientation: "portrait",
---
+++ apps/web/src/lib/sw.ts
@@ -0,0 +1,31 @@
+import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
+
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
+  clientsClaim: true,
+  fallbacks: {
+    entries: [
+      {
+        matcher({ request }) {
+          return request.destination === "document";
+        },
+        url: "/~offline",
+      },
+    ],
+  },
+  navigationPreload: true,
+  precacheEntries: self.__SW_MANIFEST,
+  runtimeCaching: defaultCache,
+  skipWaiting: true,
+});
+
+serwist.addEventListeners();
